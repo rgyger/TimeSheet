b5672e53d8928607b253d7f1bdc03dec3bf37fc3
Richard Gyger <rgyger@gmail.com>
2023-03-16 20:27:02 -0500

    Project Creation

---
+++ DotNet/Services.TimeSheet.Server/Controllers/TimeSheetController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Services.TimeSheet.Server.Controllers
+{
+   [ApiController]
+   [Route("[controller]")]
+   public class TimeSheetController : ControllerBase
+   {
+      private readonly ILogger<TimeSheetController> _logger;
+
+      public TimeSheetController(ILogger<TimeSheetController> logger)
+      {
+         _logger = logger;
+      }
+
+      [HttpGet]
+      [Route("{accountId}")]
+      [ProducesResponseType(StatusCodes.Status200OK)]
+      [ProducesResponseType(StatusCodes.Status204NoContent)]
+      [ProducesResponseType(StatusCodes.Status400BadRequest)]
+      public async Task<IActionResult> Get()
+      {
+         
+      }
+   }
+}
